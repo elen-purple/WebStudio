3de07a3f12597bf0bde2eaa9fc4bed345529ad40
elen-purple <anna.havrylova.2011@gmail.com>
2024-01-15 08:44:09 +0200

    update modal

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector(".backdrop"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.backdrop.addEventListener("click", toggleModal);
 
-  function toggleModal() {
+  function toggleModal(e) {
+    if (e.currentTarget === refs.backdrop && e.target !== e.currentTarget)
+      return;
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
   }
